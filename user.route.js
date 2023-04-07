77a525272d139dda2fd66cd2b7d9104e33a7ff5e
LiliAzatyan <109226468+LiliAzatyan@users.noreply.github.com>
2023-04-07 22:06:08 +0400

    Update user.route.js

--- user.route.js
+++ user.route.js
@@ -3,9 +3,25 @@ import express from "express";
 export const route = express.Router();
 
 route.get('/', (req, res)=>{
-    res.send("Hi user")
+    try {
+        res.send("Hi user")
+    } catch (error) {
+        console.log(new Error("error code"));
+    }
 })
 
 route.post('/', (req, res)=>{
-    res.send(req.body.name)
+    try {
+        res.send(req.body.name)
+    } catch (error) {
+        console.log(new Error("error code"));
+    }
+})
+
+route.delete('/', (req, res) => {
+    try {
+        res.send(req.body.data["this resource"]);
+    } catch (error) {
+        console.log(new Error("error code"));
+    }
 })
